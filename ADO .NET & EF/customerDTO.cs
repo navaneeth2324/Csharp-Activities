731f63a60c9c7644ceacdb1f6806e46317edd736
Navaneeth <51811219+navaneeth2324@users.noreply.github.com>
2021-09-17 00:03:04 +0530

    customerDTO.cs
    Data transfer object

---
+++ ADO .NET & EF/customerDTO.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDTO
+{
+    public class customerDTO
+    {
+        public int Emp_id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string Contact_number { get; set; }
+        public string Email_address { get; set; }
+        
+     
+
+    }
+}
