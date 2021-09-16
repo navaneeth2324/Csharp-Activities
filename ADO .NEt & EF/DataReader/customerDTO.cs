cabd6524a24b59550697ecf9edfe9e17cc0ea5bf
Navaneeth <51811219+navaneeth2324@users.noreply.github.com>
2021-09-16 20:18:12 +0530

    DTo file

---
+++ ADO .NEt & EF/DataReader/customerDTO.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDTO
+{
+    public class customerDTO
+    {
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+    }
+}
